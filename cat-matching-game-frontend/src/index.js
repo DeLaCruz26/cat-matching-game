28815dba28c45639f3c21653e003863b7dad3809
sarisgar28 <sarisgar28@gmail.com>
2021-01-25 16:24:09 -0500

    commit

--- cat-matching-game-frontend/src/index.js
+++ cat-matching-game-frontend/src/index.js
@@ -1,4 +1,8 @@
-const BACKEND_URL = 'localhost:3000';
-fetch(`${BACKEND_URL}/test`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+// const BACKEND_URL = 'localhost:3000';
+// fetch(`${BACKEND_URL}/test`)
+//   .then(response => response.json())
+//   .then(parsedResponse => console.log(parsedResponse));
+
+function memoryCards(){
+    document.getElementsByClassName("catcard");
+}
