6ad63612d387f6186cb051780bfba552166fd727
sarisgar28 <sarisgar28@gmail.com>
2021-02-05 11:31:46 -0500

    tried to do some css

--- cat-matching-game-frontend/src/score.js
+++ cat-matching-game-frontend/src/score.js
@@ -5,22 +5,22 @@ class Score{
 
         api.fetchUsers().then((users)=> {
             users.forEach(user => {
-                const li = document.createElement("li")
-                li.className = "user-list"
+                const ol = document.createElement("ol")
+                ol.className = "user-list"
                 user.scores.forEach(score =>{
                     const p = document.createElement("p")
                     p.className = "score-list"
-                    p.innerText = `Username: ${user.username}- Score:${score.user_score}`
-                    li.append(p)
+                    p.innerText = `USERNAME: ${user.username}- SC:${score.user_score}`
+                    ol.append(p)
                 })
-                main.append(li)
+                main.append(ol)
             
             }) 
-            const resetButton = document.createElement("BUTTON")
-            resetButton.innerText = "Reset Game"
-            main.append(resetButton)
+            const newGame = document.createElement("BUTTON")
+         newGame.innerText = "New Game"
+            main.append(newGame)
            
-            resetButton.addEventListener("click",() =>{
+         newGame.addEventListener("click",() =>{
                 this.reset()
             })
         })
@@ -32,11 +32,11 @@ class Score{
             main.innerHTML = ""
             newImagArray.forEach(newPic=> new Cat(newPic)) 
     }
-    // static renderScores = () =>{
-    //     this.scoresArea = document.createElement("div")
-    //     this.scoresArea.className = "scoresArea"
-    //     console.log(score)
-    // }
+    static renderScores = () =>{
+        this.scoresArea = document.createElement("div")
+        this.scoresArea.className = "scoresArea"
+      
+    }
 
 }
 // create a div class, p,  append it to the body
